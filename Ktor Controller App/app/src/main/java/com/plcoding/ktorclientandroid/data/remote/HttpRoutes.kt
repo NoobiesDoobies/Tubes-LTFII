05dc2fadc09d56b5b67c008d378d55ac7faf308c
NoobiesDoobies <carlioseryan20@gmail.com>
2023-04-06 21:48:31 +0700

    delete app yg rusak

--- Ktor Controller App/app/src/main/java/com/plcoding/ktorclientandroid/data/remote/HttpRoutes.kt
+++
@@ -1,7 +0,0 @@
-package com.plcoding.ktorclientandroid.data.remote
-
-object HttpRoutes {
-
-    private const val BASE_URL = "https://jsonplaceholder.typicode.com"
-    const val POSTS = "$BASE_URL/posts"
-}
