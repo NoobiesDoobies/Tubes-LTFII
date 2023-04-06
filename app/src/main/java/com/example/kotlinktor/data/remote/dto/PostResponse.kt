5a9b6c72e0f88753ab9ca4f217e9b4a99326c1e8
NoobiesDoobies <carlioseryan20@gmail.com>
2023-04-06 21:49:14 +0700

    rename folder

--- app/src/main/java/com/example/kotlinktor/data/remote/dto/PostResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.example.kotlinktor.data.remote.dto
-import kotlinx.serialization.Serializable
-@Serializable
-data class PostResponse(
-    val body: String,
-    val title: String,
-    val id: Int,
-    val userId;
-)
